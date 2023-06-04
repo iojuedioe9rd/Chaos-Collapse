a715c93fe700aa7d16daa492cb42ba979d963887
iojuedioe9rd <catsstudio81@gmail.com>
2023-06-04 11:52:08 +0100

    update

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -15,6 +15,18 @@ namespace Player
         public float turnSmoothTime = .1f;
         float turnSmoothVel;
 
+        public Transform jumpPos;
+
+        bool Jumping;
+        public float maxJumpTime = 10;
+        public float minJumpTime = 1;
+        public float jump = 1;
+        public float maxJump = 100;
+        public float minJump = 1;
+        float jumpTime;
+
+        float t = 0;
+
         // Start is called before the first frame update
         void Start()
         {
@@ -27,16 +39,40 @@ namespace Player
             float horizontal = Input.GetAxis("Horizontal");
             float vertical = Input.GetAxis("Vertical");
 
-            Vector3 dir = new Vector3(horizontal, 0, vertical).normalized;
+            Vector3 dir = new Vector3(horizontal, vertical).normalized;
+
+            jumpTime = Mathf.Lerp(minJumpTime, maxJumpTime, t);
+            t += Time.deltaTime;
+            print( Mathf.Lerp(minJump, maxJump, jumpTime));
+            
+
             if (dir.magnitude >= .1f)
             {
                 float Angle = Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
                 float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, Angle, ref turnSmoothVel, turnSmoothTime);
                 transform.rotation = Quaternion.Euler(0, angle, 0);
 
+                
                 Vector3 moveDir = Quaternion.Euler(0f, Angle, 0f) * Vector3.forward;
-                rb.MovePosition(transform.position + (speed * Time.deltaTime * moveDir.normalized));
+                if (Input.GetKeyDown(KeyCode.Space) && isJumping())
+                {
+                    Vector3 v = new Vector3(0, 1000, 0);
+                    rb.AddForce(v * Time.deltaTime);
+
+                    moveDir.y = v.y;
+                }
+                rb.MovePosition(transform.position + (speed * Time.deltaTime * (moveDir.normalized)));
             }
+        }
+
+        bool isJumping()
+        {
+            if (Physics.Raycast(jumpPos.position, Vector3.down, out RaycastHit hit, 5f))
+            {
+                return true;
+            }
+
+            return false;
         }
     }
 }
